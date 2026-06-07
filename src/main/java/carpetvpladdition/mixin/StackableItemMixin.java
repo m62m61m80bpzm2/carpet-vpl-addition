2108300a853bc233f13dd8c8903d4e1769469920
m62m61m80bpzm2 <13137266511@163.com>
2026-06-07 16:52:58 +0800

    1.1.0

---
+++ src/main/java/carpetvpladdition/mixin/StackableItemMixin.java
@@ -0,0 +1,25 @@
+package carpetvpladdition.mixin;
+
+import carpetvpladdition.settings.CarpetVPLAdditionSettings;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public abstract class StackableItemMixin {
+    @Inject(method = "getMaxStackSize", at = @At("HEAD"), cancellable = true)
+    private void modifyMaxStackSize(CallbackInfoReturnable<Integer> cir) {
+        ItemStack self = (ItemStack) (Object) this;
+
+        if (CarpetVPLAdditionSettings.stackableTotem && self.is(Items.TOTEM_OF_UNDYING)) {
+            cir.setReturnValue(64);
+        }
+
+        if (CarpetVPLAdditionSettings.stackableLavaBucket && self.is(Items.LAVA_BUCKET)) {
+            cir.setReturnValue(64);
+        }
+    }
+}
